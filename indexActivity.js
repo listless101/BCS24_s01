a4bd726edf51e3ce4d1cea5be2c4f1a4a0d75029
listless101 <angelofacto@gmail.com>
2023-03-21 11:22:36 +0800

    Add MainActivity

---
+++ indexActivity.js
@@ -0,0 +1,19 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'John Angelo';
+let lastName = 'Facto';
+let age = 20;
+let hobbies = ['Gaming','Sleeping','Listening to Music','Watching yt Videos','Eating'];
+let homeAddress = {
+    houseNumber: 023,
+    street: 'AveMaria',
+    subdivision: 'Norvioric',
+    barangay: 'SilanganII',
+    city: 'Rosario',
+    province: 'Cavite',
+    zipcode: 4106
+}
+console.log(`${firstName} ${lastName}`);
+console.log(`Age: ${age}`);
+console.log(hobbies);
+console.log(homeAddress);
